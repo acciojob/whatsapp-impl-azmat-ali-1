55cab314b5adb22bbac9f8081d56dcdaad374e2f
AZMAT ALI <222azmat@gmail.com>
2023-01-28 14:57:39 +0530

    2

--- src/main/java/com/driver/User.java
+++ src/main/java/com/driver/User.java
@@ -3,4 +3,9 @@ package com.driver;
 public class User {
     private String name;
     private String mobile;
+
+    public User(String name, String mobile) {
+        this.name = name;
+        this.mobile = mobile;
+    }
 }
